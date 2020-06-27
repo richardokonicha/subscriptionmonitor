c48bcde1cf5a0b3b0ef2fa6a746a12faf529401a
Richard Okonicha <Richard Okonicha>
2020-06-27 11:24:05 +0100

    woo api

--- api_methods.py
+++ api_methods.py
@@ -3,9 +3,12 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-ckey = os.getenv("ckey")
-csecret = os.getenv("csecret")
-url = "www.bst-forexgroup.com"
+# ckey = os.getenv("ckey")
+# csecret = os.getenv("csecret")
+url = "https://www.bst-forexgroup.com/"
+
+ckey = "ck_b84a2b10b2ddd451f10306100e54f7c505e3cb76"
+csecret = "cs_acb181069525e833b00b90c0b8e6139da37f0b87"
 
 wcapi = API(
     url=url,
@@ -13,3 +16,6 @@ wcapi = API(
     consumer_secret=csecret,
     version="wc/v3"
 )
+
+r = wcapi.get("products")
+print(r.status_code)
